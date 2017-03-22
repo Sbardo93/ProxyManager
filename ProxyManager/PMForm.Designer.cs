be9745e2a1dcec9076859e8bb38ca97ac1e5e602
Sbardo93 <andrea.sba1993@gmail.com>
2017-03-22 16:04:35 +0100

    proxy manager

---
+++ ProxyManager/PMForm.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace ProxyManager
+{
+    partial class PMForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PMForm));
+            this.btnSetProxy = new System.Windows.Forms.Button();
+            this.lblProxy = new System.Windows.Forms.Label();
+            this.txtProxy = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblCurrentProxy = new System.Windows.Forms.Label();
+            this.lblCurrentProxyValue = new System.Windows.Forms.Label();
+            this.chkProxyEnabled = new System.Windows.Forms.CheckBox();
+            this.txtExceptions = new System.Windows.Forms.TextBox();
+            this.lblExceptions = new System.Windows.Forms.Label();
+            this.btnSelectProxy = new System.Windows.Forms.Button();
+            this.ddlProxy = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnSetProxy
+            // 
+            this.btnSetProxy.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSetProxy.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnSetProxy.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetProxy.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSetProxy.Location = new System.Drawing.Point(303, 206);
+            this.btnSetProxy.Name = "btnSetProxy";
+            this.btnSetProxy.Size = new System.Drawing.Size(83, 33);
+            this.btnSetProxy.TabIndex = 0;
+            this.btnSetProxy.Text = "Set Proxy";
+            this.btnSetProxy.UseVisualStyleBackColor = false;
+            this.btnSetProxy.Click += new System.EventHandler(this.btnSetProxy_Click);
+            // 
+            // lblProxy
+            // 
+            this.lblProxy.AutoSize = true;
+            this.lblProxy.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProxy.ForeColor = System.Drawing.Color.Snow;
+            this.lblProxy.Location = new System.Drawing.Point(13, 29);
+            this.lblProxy.Name = "lblProxy";
+            this.lblProxy.Size = new System.Drawing.Size(39, 15);
+            this.lblProxy.TabIndex = 1;
+            this.lblProxy.Text = "Proxy:";
+            // 
+            // txtProxy
+            // 
+            this.txtProxy.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProxy.Location = new System.Drawing.Point(56, 26);
+            this.txtProxy.Name = "txtProxy";
+            this.txtProxy.Size = new System.Drawing.Size(330, 23);
+            this.txtProxy.TabIndex = 2;
+            this.txtProxy.WordWrap = false;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(56, 52);
+            this.txtPort.MaxLength = 4;
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(47, 23);
+            this.txtPort.TabIndex = 4;
+            this.txtPort.WordWrap = false;
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPort.ForeColor = System.Drawing.Color.Snow;
+            this.lblPort.Location = new System.Drawing.Point(20, 55);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(32, 15);
+            this.lblPort.TabIndex = 3;
+            this.lblPort.Text = "Port:";
+            // 
+            // lblCurrentProxy
+            // 
+            this.lblCurrentProxy.AutoSize = true;
+            this.lblCurrentProxy.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentProxy.ForeColor = System.Drawing.Color.Snow;
+            this.lblCurrentProxy.Location = new System.Drawing.Point(13, 7);
+            this.lblCurrentProxy.Name = "lblCurrentProxy";
+            this.lblCurrentProxy.Size = new System.Drawing.Size(82, 15);
+            this.lblCurrentProxy.TabIndex = 5;
+            this.lblCurrentProxy.Text = "Current Proxy:";
+            // 
+            // lblCurrentProxyValue
+            // 
+            this.lblCurrentProxyValue.AutoSize = true;
+            this.lblCurrentProxyValue.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentProxyValue.ForeColor = System.Drawing.Color.Snow;
+            this.lblCurrentProxyValue.Location = new System.Drawing.Point(92, 7);
+            this.lblCurrentProxyValue.Name = "lblCurrentProxyValue";
+            this.lblCurrentProxyValue.Size = new System.Drawing.Size(41, 15);
+            this.lblCurrentProxyValue.TabIndex = 6;
+            this.lblCurrentProxyValue.Text = "_proxy";
+            // 
+            // chkProxyEnabled
+            // 
+            this.chkProxyEnabled.AutoSize = true;
+            this.chkProxyEnabled.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkProxyEnabled.ForeColor = System.Drawing.Color.Snow;
+            this.chkProxyEnabled.Location = new System.Drawing.Point(163, 214);
+            this.chkProxyEnabled.Name = "chkProxyEnabled";
+            this.chkProxyEnabled.Size = new System.Drawing.Size(100, 19);
+            this.chkProxyEnabled.TabIndex = 7;
+            this.chkProxyEnabled.Text = "Proxy Enabled";
+            this.chkProxyEnabled.UseVisualStyleBackColor = true;
+            this.chkProxyEnabled.CheckedChanged += new System.EventHandler(this.chkProxyEnabled_CheckedChanged);
+            // 
+            // txtExceptions
+            // 
+            this.txtExceptions.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtExceptions.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExceptions.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtExceptions.Location = new System.Drawing.Point(16, 99);
+            this.txtExceptions.Multiline = true;
+            this.txtExceptions.Name = "txtExceptions";
+            this.txtExceptions.Size = new System.Drawing.Size(370, 102);
+            this.txtExceptions.TabIndex = 8;
+            this.txtExceptions.TextChanged += new System.EventHandler(this.txtExceptions_TextChanged);
+            // 
+            // lblExceptions
+            // 
+            this.lblExceptions.AutoSize = true;
+            this.lblExceptions.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExceptions.ForeColor = System.Drawing.Color.Snow;
+            this.lblExceptions.Location = new System.Drawing.Point(16, 80);
+            this.lblExceptions.Name = "lblExceptions";
+            this.lblExceptions.Size = new System.Drawing.Size(66, 15);
+            this.lblExceptions.TabIndex = 9;
+            this.lblExceptions.Text = "Exceptions:";
+            // 
+            // btnSelectProxy
+            // 
+            this.btnSelectProxy.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSelectProxy.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnSelectProxy.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectProxy.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSelectProxy.Location = new System.Drawing.Point(303, 56);
+            this.btnSelectProxy.Name = "btnSelectProxy";
+            this.btnSelectProxy.Size = new System.Drawing.Size(83, 33);
+            this.btnSelectProxy.TabIndex = 10;
+            this.btnSelectProxy.Text = "Select";
+            this.btnSelectProxy.UseVisualStyleBackColor = false;
+            this.btnSelectProxy.Click += new System.EventHandler(this.btnSelectProxy_Click);
+            // 
+            // ddlProxy
+            // 
+            this.ddlProxy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddlProxy.FormattingEnabled = true;
+            this.ddlProxy.Location = new System.Drawing.Point(197, 63);
+            this.ddlProxy.Name = "ddlProxy";
+            this.ddlProxy.Size = new System.Drawing.Size(100, 21);
+            this.ddlProxy.TabIndex = 11;
+            // 
+            // PMForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(398, 242);
+            this.Controls.Add(this.ddlProxy);
+            this.Controls.Add(this.btnSelectProxy);
+            this.Controls.Add(this.lblExceptions);
+            this.Controls.Add(this.txtExceptions);
+            this.Controls.Add(this.chkProxyEnabled);
+            this.Controls.Add(this.lblCurrentProxyValue);
+            this.Controls.Add(this.lblCurrentProxy);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.txtProxy);
+            this.Controls.Add(this.lblProxy);
+            this.Controls.Add(this.btnSetProxy);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PMForm";
+            this.Text = "ProxyManager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSetProxy;
+        private System.Windows.Forms.Label lblProxy;
+        private System.Windows.Forms.TextBox txtProxy;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblCurrentProxy;
+        private System.Windows.Forms.Label lblCurrentProxyValue;
+        private System.Windows.Forms.CheckBox chkProxyEnabled;
+        private System.Windows.Forms.TextBox txtExceptions;
+        private System.Windows.Forms.Label lblExceptions;
+        private System.Windows.Forms.Button btnSelectProxy;
+        private System.Windows.Forms.ComboBox ddlProxy;
+    }
+}
+
---
+++ ProxyManager/PMForm.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Xml;
+
+namespace ProxyManager
+{
+    public partial class PMForm : Form
+    {
+        #region --------- Properties and Settings ---------
+        [DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        public const int INTERNET_OPTION_REFRESH = 37;
+        const string ProxyEnableRegKey = "ProxyEnable";
+        const string ProxyOverrideRegKey = "ProxyOverride";
+        const string ProxyServerRegKey = "ProxyServer";
+        static bool settingsReturn, refreshReturn;
+        RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+        private Proxy _currentProxy;
+        public string CurrentProxy
+        {
+            get
+            {
+                if (_currentProxy == null)
+                    _currentProxy = new Proxy();
+                return _currentProxy.ToString();
+            }
+            set
+            {
+                _currentProxy = new Proxy(value);
+                lblCurrentProxyValue.Text = CurrentProxy;
+            }
+        }
+        public string CurrentProxyExceptions
+        {
+            set
+            {
+                if (_currentProxy != null)
+                    _currentProxy._exceptions = value;
+                txtExceptions.Text = value;
+            }
+        }
+        public bool CurrentProxyEnabled
+        {
+            get { return chkProxyEnabled.Checked; }
+            set { chkProxyEnabled.Checked = value; }
+        }
+
+        List<Proxy> ProxyList = new List<Proxy>();
+        #endregion
+
+        public PMForm()
+        {
+            InitializeComponent();
+            try
+            {
+                CurrentProxy = registry.GetValue(ProxyServerRegKey) + "";
+                CurrentProxyEnabled = (int.Parse(registry.GetValue(ProxyEnableRegKey) + "") == 1 ? true : false);
+                CurrentProxyExceptions = registry.GetValue(ProxyOverrideRegKey) + "";
+                LoadXML();
+            }
+            catch (Exception ex) { Log(ex); }
+        }
+        private void LoadXML()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("ProxyList.xml");
+
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                if (node.HasChildNodes)
+                {
+                    ProxyList.Add(new Proxy()
+                    {
+                        _nome = node["name"].InnerText,
+                        _proxy = node["address"].InnerText,
+                        _port = node["port"].InnerText,
+                        _exceptions = node["exceptions"].InnerText
+                    });
+                    ddlProxy.Items.Add(node["name"].InnerText);
+                }
+            }
+            ddlProxy.SelectedIndex = 0;
+        }
+        private void chkProxyEnabled_CheckedChanged(object sender, EventArgs e)
+        {
+            if ((sender as CheckBox) != null)
+                SetProxyEnabled((sender as CheckBox).Checked);
+        }
+        private void btnSetProxy_Click(object sender, EventArgs e)
+        {
+            SetProxy(txtProxy.Text, txtPort.Text);
+        }
+        private void btnSelectProxy_Click(object sender, EventArgs e)
+        {
+            Proxy proxy = ProxyList.Where(x => x._nome == ddlProxy.SelectedItem.ToString()).First();
+            txtProxy.Text = proxy._proxy;
+            txtPort.Text = proxy._port;
+            txtExceptions.Text = proxy._exceptions;
+        }
+
+        #region ------------------ Utils ------------------
+        private void SetProxy(string newProxy, string newPort)
+        {
+            try
+            {
+                string proxy = newProxy;
+                if (string.IsNullOrEmpty(proxy))
+                    return;
+                if (!string.IsNullOrEmpty(newPort))
+                    proxy += ":" + newPort.Trim();
+                registry.SetValue(ProxyServerRegKey, proxy);
+                CurrentProxy = registry.GetValue(ProxyServerRegKey) + "";
+
+                RefreshSettings();
+            }
+            catch (Exception ex) { Log(ex); }
+        }
+        private void SetProxyEnabled(bool enabled)
+        {
+            try
+            {
+                registry.SetValue(ProxyEnableRegKey, enabled ? 1 : 0);
+                CurrentProxyEnabled = enabled;
+
+                RefreshSettings();
+            }
+            catch (Exception ex) { Log(ex); }
+        }
+        private void RefreshSettings()
+        {
+            // These lines implement the Interface in the beginning of program 
+            // They cause the OS to refresh the settings, causing IP to really update
+            settingsReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            refreshReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+        }
+        private void Log(Exception ex)
+        {
+            string textMess = string.Empty;
+            string name = string.Empty;
+            try
+            {
+                System.Diagnostics.StackTrace st = new System.Diagnostics.StackTrace();
+                System.Diagnostics.StackFrame sf = st.GetFrame(2);
+                name = sf.GetMethod().Name;
+            }
+            catch (Exception) { name = "Method"; }
+            textMess = "Exception in Method: " + name + "\r\n" + ex.ToString();
+            int i = 0;
+            while (i < 3)
+            {
+                if (ex.InnerException == null)
+                    break;
+                ex = ex.InnerException;
+                textMess += "\r\n" + ex.ToString();
+                i++;
+            }
+            MessageBox.Show(textMess, "ProxyManager Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        #endregion
+
+        private void txtExceptions_TextChanged(object sender, EventArgs e)
+        {
+            CurrentProxyExceptions = txtExceptions.Text;
+        }
+
+        protected class Proxy
+        {
+            public string _nome { get; set; }
+            public string _proxy { get; set; }
+            public string _port { get; set; }
+            public string _exceptions { get; set; }
+            public Proxy()
+            {
+                _proxy = string.Empty;
+                _port = string.Empty;
+            }
+            public Proxy(string proxyReg)
+            {
+                if (!string.IsNullOrEmpty(proxyReg))
+                {
+                    string[] proxy = proxyReg.Split(':');
+                    if (proxy.Length > 0)
+                        this._proxy = proxy[0];
+                    if (proxy.Length > 1)
+                        this._port = proxy[1];
+                }
+            }
+            public override string ToString()
+            {
+                string proxy = (_proxy + "").Trim();
+                if (!string.IsNullOrEmpty(_port))
+                    proxy += ":" + _port.Trim();
+                return proxy;
+            }
+        }
+    }
+}
